d4d6baa9889cbc3abaf5f9485b8ebb239eb5ede9
Iftekhar <iftekharmail1@gmail.com>
2019-04-29 01:31:02 -0700

    First commit

---
+++ js/tulsa-home.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($){
+
+    // $('.nstSlider').nstSlider({
+    //     "left_grip_selector": ".leftGrip",
+    //     "right_grip_selector": ".rightGrip",
+    //     "value_bar_selector": ".bar",
+    //     "value_changed_callback": function(cause, leftValue, rightValue) {
+    //         $(this).parent().find('.leftLabel').text(leftValue);
+    //         $(this).parent().find('.rightLabel').text(rightValue);
+    //     }
+    // });
+
+
+    // $("#range_01").ionRangeSlider({
+    //     type: "single",
+    //     min: 150,
+    //     max: 250,
+    //     from: 200,
+    //     to: 250,
+    //     prefix: "£",
+    //     step: 1,
+    // });
+    
+
+});
